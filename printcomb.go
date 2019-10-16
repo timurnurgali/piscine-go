5f39f54bff18d61ebdbfe6ee9d6fa09708731471
timurnurgali <timur3112@gmail.com>
2019-10-16 22:16:20 +0600

    printcomb

--- printcomb.go
+++ printcomb.go
@@ -4,12 +4,12 @@ import "github.com/01-edu/z01"
 
 func PrintComb() {
 
-	for i := 0; i <= 7; i++ {
-		z01.PrintRune(rune(i))
-		for j := 1; 8 >= j > i; j++ {
-			z01.PrintRune(rune(j))
-			for k := 2; 9 >= k > j; k++ {
-				z01.PrintRune(rune(k + ',' + ' '))
+	for i := '0'; i <= '7'; i++ {
+		z01.PrintRune(i)
+		for j := '1'; '8' >= j > i; j++ {
+			z01.PrintRune(j)
+			for k := '2'; '9' >= k > j; k++ {
+				z01.PrintRune(k + ',' + ' ')
 			}
 		}
 	}
