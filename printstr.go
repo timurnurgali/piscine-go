8279fd5272cb1b8803ae4cf5163850e3f26837b4
timurnurgali <timur3112@gmail.com>
2019-10-17 20:50:24 +0600

    printstr

--- printstr.go
+++ printstr.go
@@ -3,9 +3,8 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintStr(str string) {
-	slice := []byte(str)
-	ln := len(str)
-	for i := 0; i < ln; i++ {
-		z01.PrintRune(rune(slice[i]))
+	sl := []byte(str)
+	for _, letter := range sl {
+		z01.PrintRune(rune(letter))
 	}
 }
