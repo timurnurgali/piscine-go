1985ce05ae8dcedffee1f0a6f349d4433c4b0a75
timurnurgali <timur3112@gmail.com>
2019-10-18 11:07:33 +0600

    printnbr

--- printnbr.go
+++ printnbr.go
@@ -3,5 +3,27 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintNbr(n int) {
-	z01.PrintRune(int32(n))
+	if n < 0 {
+		z01.PrintRune('-')
+	}
+	type(n)
+}
+
+func type(nbr int) {
+	digit := '0'
+	if nbr == 0 {
+		z01.PrintRune(digit)
+		return
+	}
+	for i := 1 ; i <= nbr%10; i++ {
+		digit++
+	}
+	for i := -1 ; i >= nbr%10; i-- {
+		digit++
+	}
+	if nbr/10 != 0 {
+		type(nbr / 10)
+	}
+	z01.PrintRune(digit)
+	return
 }
