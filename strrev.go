0770487b3871a362d8e4280e25a826ab93b8f2f7
timurnurgali <timur3112@gmail.com>
2019-10-18 19:24:20 +0600

    strrev

--- strrev.go
+++ strrev.go
@@ -1,17 +1,17 @@
 package piscine
 
-//import "github.com/01-edu/z01"
+//import "github.com/01-edu/z01" NOT FINISHED YET
 
 func StrRev(s string) string {
 	sl := []byte(s)
-	var newS string
+	var newS []byte
 	ln := 0
 	for range s {
 		ln += 1
 		//z01.PrintRune(rune(sl[i]))
 	}
-	for _, letter := range sl {
-		newS += string(sl[letter])
+	for i := ln - 1 ; i >= 0 ; i-- {
+		newS += sl[i]
 	}
-	return newS
+	return string(newS)
 }
