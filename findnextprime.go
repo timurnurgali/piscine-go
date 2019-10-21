8763196b4922c43c8ae0c2406c969e0e2f6c42ed
timurnurgali <timur3112@gmail.com>
2019-10-21 20:19:29 +0600

    quest-04-8

---
+++ findnextprime.go
@@ -0,0 +1,26 @@
+// package piscine
+
+package main
+
+func FindNextPrime(nb int) int {
+	if nb <= 2 {
+		return 2
+	}
+	a := 0
+	for i := nb; i > 2; i++ {
+		for k := 2; k < i; k++ {
+
+			if i%k == 0 {
+				return FindNextPrime(nb + 1)
+			}
+
+		}
+		a = i
+		break
+	}
+	return a
+}
+
+func main() {
+	println(FindNextPrime(2))
+}
