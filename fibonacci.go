cc9f4939af518f5df21740af12c9db8788ff96ba
timurnurgali <timur3112@gmail.com>
2019-10-21 17:03:51 +0600

    quest-04-5

---
+++ fibonacci.go
@@ -0,0 +1,22 @@
+package piscine
+
+// package main
+
+func Fibonacci(index int) int {
+
+	if index == 0 {
+		return 0
+	}
+	if index == 1 {
+		return 1
+	}
+	if index < 0 {
+		return -1
+	}
+	return Fibonacci(index-1) + Fibonacci(index-2)
+
+}
+
+// func main() {
+// 	println(Fibonacci(-7))
+// }
