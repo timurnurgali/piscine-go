0ac7d55d9998a97c7e2c1a9fecc25af0faf1fcc3
timurnurgali <timur3112@gmail.com>
2019-10-23 20:23:39 +0600

    quest-05-5

---
+++ nrune.go
@@ -0,0 +1,13 @@
+// package main
+package piscine
+
+// import "github.com/01-edu/z01"
+
+func NRune(s string, n int) rune {
+	srun := []rune(s)
+	return srun[n-1]
+}
+
+// func main() {
+// 	z01.PrintRune(NRune("Timur",4)) //u index 3
+// }
