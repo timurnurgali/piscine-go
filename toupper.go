e6bdc8955171c6c22c2e2812b11398f96a4a2866
timurnurgali <timur3112@gmail.com>
2019-10-23 22:16:36 +0600

    quest-05-9

--- toupper.go
+++ toupper.go
@@ -1,8 +1,8 @@
 package piscine
 
 func ToUpper(s string) string {
-	runes := []rune(s)
-	diff := 'A' - 'a'
+	a := []rune(s)
+	b := 'A' - 'a'
 	for i := range runes {
 		if runes[i] >= 'a' && runes[i] <= 'z' {
 			runes[i] += diff
