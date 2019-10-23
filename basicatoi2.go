a7885833203ed446769785598b73c2c230ad5428
timurnurgali <timur3112@gmail.com>
2019-10-23 12:27:09 +0600

    basicatoi2

---
+++ basicatoi2.go
@@ -0,0 +1,37 @@
+// package main
+package piscine
+
+// import "fmt"
+
+func BasicAtoi(s string) int {
+
+	ln := 0
+	num := 0
+
+	for _, strl := range s {
+		if rune(strl) < '0' || rune(strl) > '9' {
+			return 0
+		}
+		ln++
+	}
+
+	for i := 0; i < ln; i++ {
+
+		dig := 0
+
+		for j := '0'; j < rune(s[i]); j++ {
+			dig++
+		}
+		for k := 0; k < ln-i-1; k++ {
+			dig *= 10
+		}
+		num += dig
+	}
+	return num
+}
+
+// func main() {
+// 	s := "0012h3" // must return 0
+// 	n := BasicAtoi(s)
+// 	fmt.Println(n)
+// }
