4e37a132e09fbe4ca851da30b09be1553d1e06f9
timurnurgali <timur3112@gmail.com>
2019-10-23 21:01:48 +0600

    quest-05-5

--- nrune.go
+++ nrune.go
@@ -4,11 +4,12 @@ package piscine
 // import "github.com/01-edu/z01"
 
 func NRune(s string, n int) rune {
-
-	strToRune := []rune(s)
-	n2 := n - 1
-	return strToRune(n2)
-}
+	for index, r := range []rune(s) {
+		if index+1 == n {
+			return r
+		}
+	}
+	return '\x00'
 
 // func main() {
 // 	z01.PrintRune(NRune("Tadasdasdsad",0))
