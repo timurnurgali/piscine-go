266b388b97015435708d72aec9b04caae1711bda
timurnurgali <timur3112@gmail.com>
2019-10-23 20:18:41 +0600

    quest-05-4

---
+++ firstrune.go
@@ -0,0 +1,13 @@
+// package main
+package piscine
+
+import "github.com/01-edu/z01"
+
+func FirstRune(s string) rune {
+	srun := []rune(s)
+	return srun[0]
+}
+
+// func main() {
+// 	z01.PrintRune(FirstRune("Hellowwwww"))
+// }
