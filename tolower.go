4da21a33c3ed7e662d5aa922f366247084ade41a
timurnurgali <timur3112@gmail.com>
2019-10-23 22:24:10 +0600

    quest-05-10

---
+++ tolower.go
@@ -0,0 +1,12 @@
+package piscine
+
+func ToLower(s string) string {
+	slice := []rune(s)
+	b := 'A' - 'a'
+	for i := range slice {
+		if slice[i] >= 'A' && slice[i] <= 'Z' {
+			slice[i] -= diff
+		}
+	}
+	return string(slice)
+}
