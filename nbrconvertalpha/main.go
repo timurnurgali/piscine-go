55f4b13c52267a821ef37f73bbe81c2c5a22a386
timurnurgali <timur3112@gmail.com>
2019-10-24 21:21:57 +0600

    quest-06-5

---
+++ nbrconvertalpha/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+	"os"
+)
+
+func strToNum(s string) int {
+	ln := 0
+	num := 0
+	for range s {
+		ln++
+	}
+	sl := []rune(s)
+	multiplier := 1
+
+	for i := ln - 1; i >= 0; i-- {
+		x := 0
+		for j := '0'; j < sl[i]; j++ {
+			x++
+		}
+		num += x * multiplier
+		multiplier *= 10
+	}
+	return num
+}
+
+func main() {
+
+	args := os.Args[1:]
+	isUpper := false
+
+	for _, b := range args { // "12" "13"
+		if b == "--upper" {
+			isUpper = true
+		}
+		b2 := strToNum(b)
+		if b2 > 0 && b2 < 27 {
+			if isUpper {
+				z01.PrintRune(rune(b2 + 64))
+			} else {
+				z01.PrintRune(rune(b2 + 96))
+			}
+		} else {
+			z01.PrintRune(' ')
+		}
+
+	}
+
+}
