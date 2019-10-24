5dcab3f7a5a967fbed381f63c07c6f2c8b196d04
timurnurgali <timur3112@gmail.com>
2019-10-24 20:18:42 +0600

    quest-06-4

--- sortparams/main.go
+++ sortparams/main.go
@@ -9,13 +9,18 @@ func main() {
 
 	slice := os.Args[1:] //"a" "b" "1" "A"
 	var array [256]int
-
+	var runes [256]rune
 	for _, b := range slice {
-		array[[]byte(b)[0]]++
+		for _, c := range b {
+			array[[]byte(b)[0]]++
+			runes[[]byte(b)[0]] = c
+		}
 	}
+
 	for i, tick := range array {
+
 		if tick > 0 {
-			z01.PrintRune(rune(i))
+			z01.PrintRune(runes[i])
 		}
 	}
 }
